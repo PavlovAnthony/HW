da4f21d37c0cbf1059c7243e2a5aeeed2e99461f
Anton <pavlov@mail.ru>
2022-09-13 22:44:47 +0500

    ветвление

---
+++ exampe005/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "Маша")
+{
+    Console.Write("Ура! Это же Маша");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
